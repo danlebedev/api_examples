4f1ae5b9c3f11ae7873b1c9ec05796229e9b0f0c
danlebedev <lebedev.py@gmail.com>
2024-12-04 16:59:29 +0300

    Потребление RESTful API

---
+++ enroll_all.py
@@ -0,0 +1,11 @@
+import requests
+
+
+base_url = 'http://127.0.0.1:8000/api/'
+
+# Извлечь все курсы.
+r = requests.get(f'{base_url}courses/')
+courses = r.json()
+
+available_courses = ', '.join([course['title'] for course in courses])
+print(f'Available courses: {available_courses}')
